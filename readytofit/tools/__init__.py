a34f4d1ca33103cc1d81af7446a02b9e216c04fb
Fiodar Drazdou <dfgckpsk@protonmail.com>
2021-05-28 14:48:03 +0300

    fixed if log config absent

--- readytofit/tools/__init__.py
+++ readytofit/tools/__init__.py
@@ -1,9 +1,11 @@
 from .logging import _WhiteFormatter, logged, init_logger
+import logging
 from .config import Config
 import os
 
 _config = Config()
 _cfg_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "log_cfg.ini")
 if not _config.init_from(_cfg_file):
-    raise Exception(f"Failed to read config from: {_cfg_file}")
+    # raise Exception(f"Failed to read config from: {_cfg_file}")
+    _config._config["LOGGING"]["log_level"] = logging.INFO
 init_logger(_config)
