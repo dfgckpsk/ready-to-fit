6e550394c64c78d4300dcc6da995c5740a6a572c
Fiodar Drazdou <dfgckpsk@protonmail.com>
2021-05-28 14:49:20 +0300

    fixed if log config absent

--- readytofit/tools/__init__.py
+++ readytofit/tools/__init__.py
@@ -7,5 +7,6 @@ _config = Config()
 _cfg_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "log_cfg.ini")
 if not _config.init_from(_cfg_file):
     # raise Exception(f"Failed to read config from: {_cfg_file}")
+    _config._config["LOGGING"] = {}
     _config._config["LOGGING"]["log_level"] = logging.INFO
 init_logger(_config)
