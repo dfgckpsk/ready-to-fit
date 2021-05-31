cddd7604c9ae4b310cb2ab7852d1cf71afb02091
Fiodar Drazdou <dfgckpsk@protonmail.com>
2021-05-31 12:39:49 +0300

    fixed loading, drawdown positions

--- readytofit/dashboard/MLMetricsThreeVariablesPlot.py
+++ readytofit/dashboard/MLMetricsThreeVariablesPlot.py
@@ -1,6 +1,7 @@
 from .ThreeVariablesPlot import ThreeVariablesPlot, go, dash, List
 from readytofit.dashboard.MLDashboardData import MlDashboardData
 import datetime
+import time
 
 
 class MLMetricsThreeVariablesPlot(ThreeVariablesPlot):
@@ -8,21 +9,26 @@ class MLMetricsThreeVariablesPlot(ThreeVariablesPlot):
     def __init__(self, app: dash.Dash, ml_dashboard_manager: MlDashboardData):
         ThreeVariablesPlot.__init__(self, app)
         self.ml_dashboard_manager = ml_dashboard_manager
-        self.ts_metrics = None
+        self.ts_metrics = []
+        self.prev_data_id = None
+        self.already_updated = False
 
     def _get_points(self, param_name, plot_type) -> go.Scatter:
+        if len(self.ts_metrics) == 0:
+            return
         ts_metric = list(filter(lambda x: x.value_name == param_name, self.ts_metrics))[0]
         if plot_type == 'markers':
-            go.Scatter(x=ts_metric.indexes,
-                       y=ts_metric.float_values,
-                       mode='markers',
-                       name=param_name)
+            return go.Scatter(x=ts_metric.indexes,
+                              y=ts_metric.float_values,
+                              mode='markers',
+                              name=param_name)
 
         return go.Scatter(x=ts_metric.indexes,
                           y=ts_metric.float_values,
                           name=param_name)
 
-    def _update_figure(self, param_1, param_2, param_3, plot_type_1, plot_type_2, plot_type_3) -> go.Figure:
+    def _update_figure(self, param_1, param_2, param_3, plot_type_1, plot_type_2, plot_type_3, data_id) -> go.Figure:
+        self._update_ts_metrics(data_id)
         data = []
         for param, plot_type in zip([param_1, param_2, param_3], [plot_type_1, plot_type_2, plot_type_3]):
             if param is not None and plot_type is not None:
@@ -33,6 +39,20 @@ class MLMetricsThreeVariablesPlot(ThreeVariablesPlot):
 
     def _get_parameter_names_for_plot(self, data_id: str) -> List[str]:
         try:
+            self.data_id = data_id
+            self._update_ts_metrics(data_id)
+
+            return list(map(lambda x: x.value_name, self.ts_metrics))
+        except BaseException as e:
+            print(e)
+            # self.ts_metrics = None
+            return []
+
+    def _update_ts_metrics(self, data_id):
+        if data_id != self.prev_data_id and not self.already_updated:
+            self.ts_metrics = None
+            self.already_updated = True
+            self.prev_data_id = data_id
             split_num = None
             if '_' in data_id:
                 data_id, split_num = data_id.split('_')
@@ -40,12 +60,9 @@ class MLMetricsThreeVariablesPlot(ThreeVariablesPlot):
             self.ts_metrics = self.ml_dashboard_manager.get_ts_metrics(data_id)
             if split_num is not None:
                 self.ts_metrics = list(filter(lambda x: x.split_num == int(split_num), self.ts_metrics))
-
-            return list(map(lambda x: x.value_name, self.ts_metrics))
-        except BaseException as e:
-            print(e)
-            self.ts_metrics = None
-            return []
+            self.already_updated = False
+        while self.already_updated:
+            time.sleep(0.5)
 
     def _get_data_id_list(self) -> List[str]:
         import time
--- readytofit/dashboard/ThreeVariablesPlot.py
+++ readytofit/dashboard/ThreeVariablesPlot.py
@@ -1,10 +1,8 @@
 import dash_core_components as dcc
 import dash_html_components as html
-import dash_table as dtable
 from dash.dependencies import Input, Output
 from dash.exceptions import PreventUpdate
-from readytofit.db.CreatedMlModels import CreatedMlModels
-from typing import List, Tuple
+from typing import List
 from readytofit.dashboard.base.DashboardObject import DashboardObject, dash
 import plotly.graph_objs as go
 
@@ -26,55 +24,60 @@ class ThreeVariablesPlot(DashboardObject):
         self.callback_objects = [self._callback_update_data_id,
                                  self._callback_update_plot_parameter_names,
                                  self._callback_select_data_options]
+        self.data_id = None
 
     def layout(self):
 
         layout = []
 
         layout += [html.H2('ThreeVariablesPlot'),
-                   html.Button('Update runs', id=UPDATE_RUNS_ID, n_clicks=0),
-                    dcc.Loading(
-                        id="loading-3",
-                        type="circle",
-                        children=html.Div(id="loading-output-3")
-                    )]
+                   html.Button('Update runs', id=UPDATE_RUNS_ID, n_clicks=0) ]
         parameters_layouts = self._layout_data_id_drawdown()
         parameters_layouts += self._layout_plot_parameters()
         figure_layout = self._layout_figure()
         data_layout = html.Div([html.Div(figure_layout, style={'width': '70%', 'display': 'inline-block'}),
-                                html.Div(parameters_layouts, style={'width': '30%', 'display': 'inline-block'})],
+                                html.Div(parameters_layouts, style={'width': '30%',
+                                                                    'display': 'inline-block',
+                                                                    'marginBottom': 100})],
                                style={'width': '100%', 'display': 'inline-block'})
         return [html.Div(layout), data_layout]
 
     def _layout_data_id_drawdown(self):
         return [html.H2('Run id'),
-                dcc.Dropdown(id=DRAWDOWN_RUN_ID,
-                             style={'width': '90%', 'display': 'inline-block'})]
+                dcc.Loading(
+                    id="loading-4",
+                    type="circle",
+                    children=
+                    dcc.Dropdown(id=DRAWDOWN_RUN_ID,
+                             style={'width': '90%', 'display': 'inline-block'}))]
 
     def _layout_plot_parameters(self):
         plot_parameters = ['line', 'markers']
         return [html.H2('parametersss'),
                 dcc.Dropdown(id=DRAWDOWN_PARAM_1,
-                             style={'width': '50%', 'display': 'inline-block'}),
+                             style={'width': '90%', 'display': 'inline-block'}),
                 dcc.Dropdown(id=DRAWDOWN_PLOT_1,
                              options=[{'label': i, 'value': i} for i in plot_parameters],
                              value=plot_parameters[0],
-                             style={'width': '50%', 'display': 'inline-block'}),
+                             style={'width': '90%', 'display': 'inline-block'}),
                 dcc.Dropdown(id=DRAWDOWN_PARAM_2,
-                             style={'width': '50%', 'display': 'inline-block'}),
+                             style={'width': '90%', 'display': 'inline-block'}),
                 dcc.Dropdown(id=DRAWDOWN_PLOT_2,
                              options=[{'label': i, 'value': i} for i in plot_parameters],
                              value=plot_parameters[0],
-                             style={'width': '50%', 'display': 'inline-block'}),
+                             style={'width': '90%', 'display': 'inline-block'}),
                 dcc.Dropdown(id=DRAWDOWN_PARAM_3,
-                             style={'width': '50%', 'display': 'inline-block'}),
+                             style={'width': '90%', 'display': 'inline-block'}),
                 dcc.Dropdown(id=DRAWDOWN_PLOT_3,
                              options=[{'label': i, 'value': i} for i in plot_parameters],
                              value=plot_parameters[0],
-                             style={'width': '50%', 'display': 'inline-block'})]
+                             style={'width': '90%', 'display': 'inline-block'})]
 
     def _layout_figure(self):
-        return [dcc.Graph(id=FIGURE_ID)]
+        return [dcc.Loading(
+                        id="loading-3",
+                        type="circle",
+                        children=dcc.Graph(id=FIGURE_ID))]
 
     def _callback_update_data_id(self, app):
         @app.callback(Output(DRAWDOWN_RUN_ID, 'options'),
@@ -95,24 +98,26 @@ class ThreeVariablesPlot(DashboardObject):
             return paremeter_names, paremeter_names, paremeter_names
 
     def _callback_select_data_options(self, app):
-        @app.callback(Output('loading-output-3', 'children'),
-                      Output(FIGURE_ID, 'figure'),
+        @app.callback(Output(FIGURE_ID, 'figure'),
                       Input(DRAWDOWN_PARAM_1, 'value'),
                       Input(DRAWDOWN_PARAM_2, 'value'),
                       Input(DRAWDOWN_PARAM_3, 'value'),
                       Input(DRAWDOWN_PLOT_1, 'value'),
                       Input(DRAWDOWN_PLOT_2, 'value'),
-                      Input(DRAWDOWN_PLOT_3, 'value'))
-        def select_experiment(param_1, param_2, param_3, plot_type_1, plot_type_2, plot_type_3):
+                      Input(DRAWDOWN_PLOT_3, 'value'),
+                      Input(DRAWDOWN_RUN_ID, 'value'))
+        def select_experiment(param_1, param_2, param_3, plot_type_1, plot_type_2, plot_type_3, data_id):
+            print('select_experiment', param_1, param_2, param_3, plot_type_1, plot_type_2, plot_type_3)
             figure = self._update_figure(param_1,
                                          param_2,
                                          param_3,
                                          plot_type_1,
                                          plot_type_2,
-                                         plot_type_3)
+                                         plot_type_3,
+                                         data_id)
             if figure is None:
                 raise PreventUpdate
-            return True, figure
+            return figure
 
     def _get_parameter_names_for_plot(self, data_id: str) -> List[str]:
         return ['param11', 'param12', 'param13']
@@ -120,6 +125,5 @@ class ThreeVariablesPlot(DashboardObject):
     def _get_data_id_list(self) -> List[str]:
         return ['run_id1', 'run_id2', 'run_id3']
 
-    def _update_figure(self, param_1, param_2, param_3, plot_type_1, plot_type_2, plot_type_3) -> go.Figure:
-        # return go.Figure()
+    def _update_figure(self, param_1, param_2, param_3, plot_type_1, plot_type_2, plot_type_3, data_id) -> go.Figure:
         return None
